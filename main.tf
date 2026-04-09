b79d7768bf6be1b44235723e5506444095280d69
huyhcmuit <huyadchemistry123@gmail.com>
2026-04-09 11:00:42 +0700

    chore: migrate svuit-gha-logs bucket to new account

---
+++ main.tf
@@ -0,0 +1,8 @@
+#resource "cloudflare_r2_bucket" "gha_logs_bucket" {
+# account_id = "b78b9fdd13491547180d25a3c9172b30"
+#name       = "svuit-gha-logs"
+#}
+resource "cloudflare_r2_bucket" "gha_logs_backup_new" {
+  account_id = "236c34443d0b37575866b671a040ea6f"
+  name       = "svuit-gha-logs-backup"
+}
