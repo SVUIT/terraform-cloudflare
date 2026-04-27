f143cfec5aef7c8bf5b9c3b7cd21e0542d98efe8
huyhcmuit <huyadchemistry123@gmail.com>
2026-04-27 14:12:52 +0700

    feat: add links worker kv to new account (OPS-36)

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "cloudflare_r2_bucket" "gha_logs_backup_new" {
   account_id = "236c34443d0b37575866b671a040ea6f"
   name       = "svuit-gha-logs-backup"
 }
+resource "cloudflare_workers_kv_namespace" "links_kv_new" {
+  account_id = "236c34443d0b37575866b671a040ea6f"
+  title      = "links"
+}
